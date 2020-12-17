252fc0fbd0331c509143e0042f53b24027bdea28
Alexandra <kokosha117@gmail.com>
2020-12-17 16:35:56 +0300

    changed tests

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@jest/globals';
-import fs from 'fs';
+// import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../index.js';
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+// const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('flat_json', () => {
   const flatJson = `{
